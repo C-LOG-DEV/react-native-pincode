a976d96f19f633c8781fcab6af7ab89f2cd9eb63
William Bech <williambech@Williams-MacBook-Pro.local>
2019-05-02 10:01:37 +0200

    changed async-storage to native

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,5 @@
-import AsyncStorage from '@react-native-community/async-storage'
 import * as Keychain from 'react-native-keychain'
+import { AsyncStorage } from 'react-native'
 
 export enum PinResultStatus {
   initial = "initial",
